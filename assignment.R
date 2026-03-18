c3acdc88433ed6956a2e39b35bbafdd1e8e17875
James Hamilton <01-repose-alive@icloud.com>
2026-03-18 14:41:37 +0000

    added mean, variance, and histogram of residuals

--- assignment.R
+++ assignment.R
@@ -7,6 +7,12 @@ index             <- as.Date(moo[[1]], format = "%d/%m/%Y")
 cow_prices        <- as.numeric(moo[[2]]) |> unname()
 cattle            <- xts(cow_prices, order.by = index)
 colnames(cattle)  <- "daily"
+plot(cattle,
+     main = "Live Cattle Futures (SPGSLC) daily spot prices",
+     major.ticks = "years",
+     minor.ticks = FALSE,
+     format.labels = "%Y")
+
 # Log the returns
 ln_cattle <- log(cattle)
 
@@ -25,7 +31,7 @@ acf(ln_cattle, main = "ACF of log(cattle)") # Very obviously non-stationary
 adf.test(ln_cattle) # p = 0.958, no evidence against stationarity
 
 # Diff series
-ln_cattle_d <- diff(ln_cattle)
+ln_cattle_d <- diff(ln_cattle) |> na.omit()
 
 # Stationarity of diffed series
 plot(ln_cattle_d)
@@ -57,27 +63,48 @@ VaR(ln_cattle_d, p = 0.99, type = "historical") #-0.02985331
 # that may be applicable
 par(mfrow = c(2, 2))
 a <- acf(ln_cattle_d,  plot = FALSE)
-plot(a, ylim = c(-0.2, 0.2), main = "ACF") # Because acf doesn't use the ylim keyword.
+# \theta for lag 1 = 0.079
+plot(a, ylim = c(-0.2, 0.2), main = "ACF") # acf doesn't use the ylim keyword.
 p <- pacf(ln_cattle_d, ylim = c(-0.2, 0.2), main = "PACF")
+# \phi for lag 1 = 0.079
 
 # ACF and PACF are almost identical, but with a slightly significant
-# autocorrelation at lag 1. Try ARIMA(1,1,1)
+# autocorrelation at lag 1. Try AR(1), MA(1), and ARIMA(1,1,1)
 
 library(forecast)
 par(mfrow = c(1, 1))
 c <- as.numeric(cattle)
-model <- Arima(c, c(1, 1, 1))
-fc <- forecast(model, h = 12)
+
+ar <- Arima(c, c(1, 1, 0))
+ar_fc <- forecast(ar, h = 12)
+plot(ar_fc, include = 100)
+
+ma <- Arima(c, c(0, 1, 1))
+ma_fc <- forecast(ma, h = 12)
+plot(ma_fc, include = 100)
+
+arma <- Arima(c, c(1, 1, 1))
+fc <- forecast(arma, h = 12)
 plot(fc, include = 100)
 # Rubbish prediction/model. Let's look at the residuals
+ar_res <- residuals(ar)
+ma_res <- residuals(ma)
 
 # Look at residuals
 par(mfrow = c(2, 3))
-res <- residuals(model)
-plot(res)
+arima_res <- residuals(arma)
+plot(arima_res)
 # The plot appears stationary, except that the variance seems to grow over time
-adf.test(res) # p < 0.01, accept stationarity
-pacf(res, main = "PACF of Residuals")
-acf(res, main = "ACF of Residuals")
+adf.test(arima_res) # p < 0.01, accept stationarity
+pacf(arima_res, main = "PACF of Residuals")
+acf(arima_res, main = "ACF of Residuals")
+
+var(arima_res) # 20.85732
+mean(arima_res) # 0.1157353
+hist(arima_res, breaks = 50)
+
 # The residuals look like white noise, but need to double check with Ljung-Box
-Box.test(res, type = "Ljung-Box") # p = 0.8434, accept white noise.
+Box.test(arima_res, type = "Ljung-Box") # p = 0.8434, accept white noise.
+
+# Check for seasonality
+stl(ln_cattle_d[-1], s.window = "seasonal") # so unseasonal it throws an error!
